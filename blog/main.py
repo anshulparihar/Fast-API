459c717edfef9f067fc49ff5fe42b391d12a08e0
anshulparihar <anshulthedev@gmail.com>
2021-12-11 18:55:14 +0530

    getting code from database with the help of id

--- blog/main.py
+++ blog/main.py
@@ -14,12 +14,25 @@ def get_db():
         db.close()
 
 
-
+ #Creating new blog and storing it in the database
 @app.post('/blog')
-def create(request : schemas.Blog, db : Session = Depends(get_db)):
-    #Creating new blog
+def create(request : schemas.Blog, db : Session = Depends(get_db)):     #db is the database instance
+   
     new_blog = models.Blog(title = request.title,body = request.body )     #request.title because of its connection between request and schemas.py
     db.add(new_blog)    #adding new blog
     db.commit()         #commiting new blog
     db.refresh(new_blog)    #refreshing the blog
     return new_blog
+
+#getting all the blogs from the database
+@app.get('/blog')
+def getblog( db : Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+#getting blog with id
+@app.get('/blog/{id}')
+def showblog(db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    return blog 
