bf3f2332ea597e1befac3608c3e4ed6909f988ee
anshulparihar <anshulthedev@gmail.com>
2021-12-11 17:38:12 +0530

    blog created and stored in database

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 # from pydantic import BaseModel
+from sqlalchemy.orm import Session
 from . import schemas,models
-from .database import engine
-
-models.Base.metadata.create_all(bind=engine)
+from .database import engine,SessionLocal
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 @app.post('/blog')
-def create(request : schemas.Blog):
-    return request
+def create(request : schemas.Blog, db : Session = Depends(get_db)):
+    #Creating new blog
+    new_blog = models.Blog(title = request.title,body = request.body )     #request.title because of its connection between request and schemas.py
+    db.add(new_blog)    #adding new blog
+    db.commit()         #commiting new blog
+    db.refresh(new_blog)    #refreshing the blog
+    return new_blog
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from .database import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
+#Defining every field that we need in the table include table name
 class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
